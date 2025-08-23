f4c23e9cf2d0b39d9fbf9649e2a2fc046a054c1e
Chandraprakash Gupta <guptachandraprakash54@gmail.com>
2025-08-23 19:40:46 +0530

    Initial commit: portfolio

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const port = 3000
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "main", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "main", "about.html"));
+});
+
+app.get("/achivement", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "main", "achivement.html"));
+});
+
+app.get("/project", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "main", "project.html"));
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
